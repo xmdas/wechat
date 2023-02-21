6c5acf7ef1dab96ebd076cd2e5e201ef376db1d1
xmdas <125938063+xmdas@users.noreply.github.com>
2023-02-21 10:28:32 +0800

    Update server.go

--- officialaccount/server/server.go
+++ officialaccount/server/server.go
@@ -242,6 +242,7 @@ func (srv *Server) buildResponse(reply *message.Reply) (err error) {
 	case message.MsgTypeMusic:
 	case message.MsgTypeNews:
 	case message.MsgTypeTransfer:
+	case message.MsgTypeEvent:
 	default:
 		err = message.ErrUnsupportReply
 		return
